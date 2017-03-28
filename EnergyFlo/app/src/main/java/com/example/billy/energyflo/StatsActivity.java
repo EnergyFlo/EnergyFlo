1968fc53738cf05d55e28982c9e23887a56cdeb0
NamHongDo <sirsunday89@gmail.com>
2017-03-28 14:26:21 -0400

    Line Chart now pulls data from database

--- EnergyFlo/app/src/main/java/com/example/billy/energyflo/StatsActivity.java
+++ EnergyFlo/app/src/main/java/com/example/billy/energyflo/StatsActivity.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import java.util.Random;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -33,78 +33,71 @@ public class StatsActivity extends AppCompatActivity {
         mDbHelper = new DBHandler(this.getApplicationContext());
 
         android.util.Log.d("statsActivity", "Begin Logging from Stats Activity");
-
-
-//        Log thisHour = mDbHelper.getLog(15);
-//        android.util.Log.d("statsActivity", "Average for hour: " + thisHour.getHour() + " is rating: " + thisHour.getAverage());
-
-        for (hour = 0; hour<24; hour++) {
-            try {
-                thisHour = mDbHelper.getLog(hour);
-                android.util.Log.d("statsActivity", "Average for hour " + thisHour.getHour() + " is rating " + thisHour.getAverage());
-
-            }
-            catch(Exception e){
-                android.util.Log.d("statsActivity", e.toString());
-            }
-        }
-
         /*
         *
         * Graph from:  https://github.com/PhilJay/MPAndroidChart
         *
         */
+
+        //Initialize a chart
         LineChart chart = (LineChart) findViewById(R.id.chart);
-        List<Entry> averageHourRating = new ArrayList<>();
         chart.setBackgroundColor(Color.rgb(51, 51, 51));
         chart.setDrawGridBackground(false); //no grid line on background
 
+        //format X-axis
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setTextSize(20f);
-        xAxis.setTextColor(Color.rgb(0, 170, 255));
+        xAxis.setTextColor(Color.rgb(255, 255, 255));
         xAxis.setAxisMinValue(0);
         xAxis.setAxisMaximum(24);
 
+        //format Y-axis
         YAxis yAxis=chart.getAxisLeft();
         yAxis.setTextSize(20f);
-        yAxis.setTextColor(Color.rgb(0, 170, 255));
+        yAxis.setTextColor(Color.rgb(255, 255, 255));
         yAxis.setAxisMinValue(0);
         yAxis.setAxisMaximum(21);
         chart.getAxisRight().setEnabled(false);
-        /*
-        for (hour=0; hour<24;hour++){
-            thisHour = mDbHelper.getLog(hour);
-            android.util.Log.d("StatsActivity","Hour looking at: " + thisHour.getHour() +" ,The AVERAGE IS: "+thisHour.getAverage());
-            Entry entry = new Entry(thisHour.getHour(),(float)thisHour.getAverage());
-            averageHourRating.add(entry);
-        }*/
 
-        //Dummy Hours and Average Rating entry points
-        for (int i = 0; i<24; i++){
-            Random  random = new Random();
-            float averageRating= random.nextInt(20);
-            Entry ratingEntry= new Entry((float)i,averageRating);
-            averageHourRating.add(ratingEntry);
+        //Initialize a List of Entries to store all the data points
+        List<Entry> averageHourRating = new ArrayList<>();
+        //Loop through the database and add average rating on appropriate data points on line graph, in case the hour has not been recorded, rating is 0
+        //All the entries are stored in
+        for (hour=1; hour<=24;hour++){
+            try{
+                thisHour = mDbHelper.getLog(hour);
+                android.util.Log.d("StatsActivity","Hour looking at: " + thisHour.getHour() +" ,The AVERAGE IS: "+thisHour.getAverage());
+                Entry entry = new Entry(thisHour.getHour(),(float)thisHour.getAverage());
+                averageHourRating.add(entry);
+            }catch(Exception e){
+                Entry entry = new Entry(hour,0f);
+                averageHourRating.add(entry);
+            }
         }
 
-        LineDataSet setHour= new LineDataSet(averageHourRating, " Average Hour");
+        //Initialize the line with data by passing a list of data needed to be displayed
+        LineDataSet setHour= new LineDataSet(averageHourRating, " ");
+        //Formatting its elements (text, dot, line, color)
         setHour.setDrawCircles(true);
-        setHour.setLineWidth(4f);
-        setHour.setColors(Color.rgb(255, 102, 0));
+        setHour.setLineWidth(2f);
+        setHour.setColors(Color.rgb(102, 255, 255));
         setHour.setAxisDependency(YAxis.AxisDependency.LEFT);
         setHour.setMode(LineDataSet.Mode.CUBIC_BEZIER); //instead of straight lines, draw line in cubic form
-        setHour.setCircleColor(Color.rgb(0, 170, 255));
+        setHour.setCircleColor(Color.rgb(255, 255, 255));
         setHour.setCircleRadius(5f);
         setHour.setDrawCircleHole(false);
         setHour.setValueTextSize(20f);
-        setHour.setValueTextColor(Color.rgb(255, 102, 0));
+        setHour.setValueTextColor(Color.rgb(102, 255, 255));
+        setHour.setDrawValues(false);
 
-        List<ILineDataSet> dataSets= new ArrayList<ILineDataSet>();
+        //Parse the data in appropriate data set for the Line chart class
+        List<ILineDataSet> dataSets= new ArrayList<>();
         dataSets.add(setHour);
 
+        //Initialize the line chart class
         LineData data= new LineData(dataSets);
         chart.setData(data);
-        chart.invalidate();
+        chart.invalidate(); //refreshing the chart every time the chart is brought up
     }
 }
