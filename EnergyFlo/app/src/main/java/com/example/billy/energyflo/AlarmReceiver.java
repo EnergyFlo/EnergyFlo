328f4dc76201da69c15a4cd99c942a02c0649545
Dillon <dillon.purvis@gmail.com>
2017-04-25 16:16:11 -0400

    Testing recurring alarm functionality

--- EnergyFlo/app/src/main/java/com/example/billy/energyflo/AlarmReceiver.java
+++ EnergyFlo/app/src/main/java/com/example/billy/energyflo/AlarmReceiver.java
@@ -23,8 +23,8 @@ public class AlarmReceiver extends BroadcastReceiver
         // for ex you can start an activity to vibrate phone or to ring the phone
 
         Notification.Builder myBuilder = new Notification.Builder(context)
-                .setContentTitle("EnergyFlo Reminder")
-                .setContentText("Record Yoself!")
+                .setContentTitle("Reminder")
+                .setContentText("Time to record your energy level!")
                 .setSmallIcon(R.drawable.ic_menu_send)
                 .setAutoCancel(true);
 
@@ -37,6 +37,31 @@ public class AlarmReceiver extends BroadcastReceiver
         NotificationManager myNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         myNotificationManager.notify(1234, myNotification);
+
+
+//        rescheduleAlarm(context);
     }
 
+
+//    public void rescheduleAlarm(Context context)
+//    {
+//        // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time,
+//        // we fetch  the current time in milliseconds and added 1 day time
+//        // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day
+//        Long alarmTime = new GregorianCalendar().getTimeInMillis()+2000;
+//
+//        // create an Intent and set the class which will execute when Alarm triggers, here we have
+//        // given AlarmReceiver in the Intent, the onRecieve() method of this class will execute when
+//        // alarm triggers and
+//        //we will write the code to create notification inside onReceive() method pf Alarmreceiver class
+//        Intent intentAlarm = new Intent(context, AlarmReceiver.class);
+//
+//        // create the object
+//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+//
+//        //set the alarm for particular time
+//        alarmManager.set(AlarmManager.RTC_WAKEUP,alarmTime, PendingIntent.getBroadcast(context,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+//        Toast.makeText(context, "Reminder Scheduled", Toast.LENGTH_LONG).show();
+//
+//    }
 }
--- EnergyFlo/app/src/main/java/com/example/billy/energyflo/SettingsActivity.java
+++ EnergyFlo/app/src/main/java/com/example/billy/energyflo/SettingsActivity.java
@@ -4,12 +4,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.icu.util.GregorianCalendar;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity
 {
@@ -43,7 +42,12 @@ public class SettingsActivity extends AppCompatActivity
         // time at which alarm will be scheduled here alarm is scheduled at 1 day from current time,
         // we fetch  the current time in milliseconds and added 1 day time
         // i.e. 24*60*60*1000= 86,400,000   milliseconds in a day
-        Long alarmTime = new GregorianCalendar().getTimeInMillis()+2000;
+//        Long alarmTime = new GregorianCalendar().getTimeInMillis()+2000;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        android.util.Log.d("alarm", String.valueOf(calendar.getTimeInMillis()));
 
         // create an Intent and set the class which will execute when Alarm triggers, here we have
         // given AlarmReceiver in the Intent, the onRecieve() method of this class will execute when
@@ -55,8 +59,13 @@ public class SettingsActivity extends AppCompatActivity
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
         //set the alarm for particular time
-        alarmManager.set(AlarmManager.RTC_WAKEUP,alarmTime, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
-        Toast.makeText(this, "Reminder Scheduled", Toast.LENGTH_LONG).show();
+//        alarmManager.set(AlarmManager.RTC_WAKEUP,alarmTime, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+//        Toast.makeText(this, "Reminder Scheduled", Toast.LENGTH_LONG).show();
 
+
+// With setInexactRepeating(), you have to use one of the AlarmManager interval
+// constants--in this case, AlarmManager.INTERVAL_DAY.
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
     }
 }
